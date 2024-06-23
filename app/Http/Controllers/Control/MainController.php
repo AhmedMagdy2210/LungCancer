0e41c5898adbac77264f79eaf25a8a87ffc84ab0
AhmedMagdy2210 <ahmed22102002@gmail.com>
2024-06-23 08:20:31 +0300

    some changes

--- app/Http/Controllers/Control/MainController.php
+++ app/Http/Controllers/Control/MainController.php
@@ -9,21 +9,9 @@ use App\Http\Resources\RoleResource;
 
 class MainController extends Controller {
 
-    
+
     public function getAllUsers() {
-        $users = User::with('roles')->get();
-        $usersWithRoles = $users->reject(function ($user) {
-            return $user->hasRole('admin');
-        })->map(function ($user) {
-            return [
-                'user' => [
-                    'id' => $user->id,
-                    'name' => $user->name,
-                    'email' => $user->email,
-                    'roles' => RoleResource::collection($user->roles),
-                ],
-            ];
-        });
-        return response()->json(['status' => 200, 'users' => $usersWithRoles]);
+        $users = User::role('patient')->get();
+        return response()->json($users);
     }
 }
